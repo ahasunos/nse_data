deb19760795ffbbc8371f83461fa41b68c8d26db
Sonu Saha <ahasunos@gmail.com>
2024-09-04 23:07:56 +0530

    init: bundler gem nse_data & some basic add-ons like gitignore
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

---
+++ lib/nse_data/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NseData
+  VERSION = '0.1.0'
+end
---
+++ spec/nse_data_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe NseData do
+  it 'has a version number' do
+    expect(NseData::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(true).to eq(true)
+  end
+end
