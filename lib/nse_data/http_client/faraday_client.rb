1dc5a44a56abc081782fae590ea7af70309b7310
Sonu Saha <sonu.saha@progress.com>
2024-09-06 01:09:27 +0530

    chore: add comments with the help of gh copilot
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/nse_data/http_client/faraday_client.rb
+++ lib/nse_data/http_client/faraday_client.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-# lib/nse_data/http_client/faraday_client.rb
 require 'faraday'
 require 'json'
 require_relative 'base_client'
 
 module NseData
   module HttpClient
-    # FaradayClient class
+    # FaradayClient class is responsible for making HTTP requests using Faraday.
     class FaradayClient < BaseClient
+      # Sends a GET request to the specified endpoint.
+      #
+      # @param endpoint [String] The endpoint to send the request to.
+      # @return [Faraday::Response] The response object.
       def get(endpoint)
         handle_connection do |connection|
           connection.get(endpoint)
@@ -17,6 +20,12 @@ module NseData
 
       private
 
+      # Handles the connection to the base URL.
+      #
+      # @yield [connection] The Faraday connection object.
+      # @yieldparam connection [Faraday::Connection] The Faraday connection object.
+      # @return [Object] The result of the block execution.
+      # @raise [Faraday::Error] If there is an error with the connection.
       def handle_connection
         connection = Faraday.new(url: @base_url) do |faraday|
           faraday.request :json
