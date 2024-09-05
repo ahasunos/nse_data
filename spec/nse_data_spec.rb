8697fd5697135b89af2fa91b060a712c65048879
Sonu Saha <ahasunos@gmail.com>
2024-09-06 00:37:28 +0530

    add: high level methods for library users
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

--- spec/nse_data_spec.rb
+++ spec/nse_data_spec.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+require 'nse_data'
+require 'nse_data/api_manager'
+
 RSpec.describe NseData do
-  it 'has a version number' do
-    expect(NseData::VERSION).not_to be nil
+  let(:api_endpoints) do
+    {
+      "endpoint1" => { "path" => "/api/endpoint1" },
+      "endpoint2" => { "path" => "/api/endpoint2" }
+    }
   end
 
-  it 'does something useful' do
-    expect(true).to eq(true)
+  before do
+    # Mock the APIManager to control its behavior
+    api_manager_double = instance_double('NseData::APIManager')
+    allow(api_manager_double).to receive(:load_endpoints).and_return(api_endpoints)
+    allow(NseData::APIManager).to receive(:new).and_return(api_manager_double)
+  end
+
+  describe '.get_all_endpoints' do
+    it 'returns all endpoints loaded by APIManager' do
+      endpoints = NseData.get_all_endpoints
+      expect(endpoints).to eq(api_endpoints)
+    end
   end
 end
