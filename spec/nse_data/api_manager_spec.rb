d6094d112665598c9f6afb091353990566ba8dcd
Sonu Saha <ahasunos@gmail.com>
2024-09-06 12:00:44 +0530

    spec: update test as per new changes
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

--- spec/nse_data/api_manager_spec.rb
+++ spec/nse_data/api_manager_spec.rb
@@ -21,24 +21,6 @@ RSpec.describe NseData::APIManager do
     allow(NseData::HttpClient::FaradayClient).to receive(:new).with('https://www.nseindia.com/api/').and_return(faraday_client)
   end
 
-  context 'when object is initialized' do
-    describe '#initialize' do
-      it 'initializes with FaradayClient and loads endpoints' do
-        manager = described_class.new
-
-        expect(manager.instance_variable_get(:@client)).to eq(faraday_client)
-        expect(manager.instance_variable_get(:@endpoints)).to eq(api_endpoints)
-      end
-
-      it 'defines methods for each endpoint' do
-        manager = described_class.new
-
-        expect(manager).to respond_to(:get_endpoint1)
-        expect(manager).to respond_to(:get_endpoint2)
-      end
-    end
-  end
-
   describe '#fetch_data' do
     before do
       allow(faraday_client).to receive(:get).with('/api/endpoint1').and_return('data1')
