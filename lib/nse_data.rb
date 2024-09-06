36d6e570a22bd8c7582dbc40ebbd1f2cc8acfece
Sonu Saha <ahasunos@gmail.com>
2024-09-06 12:00:25 +0530

    expose methods via nse data module
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

--- lib/nse_data.rb
+++ lib/nse_data.rb
@@ -10,11 +10,21 @@ module NseData
   class Error < StandardError; end
 
   # This module provides functionality for accessing NSE data.
+  def self.define_api_methods
+    api_manager = APIManager.new
+    api_manager.endpoints.each_key do |method_name|
+      define_singleton_method("fetch_#{method_name}") do
+        api_manager.fetch_data(method_name).body
+      end
+    end
+  end
 
   # Returns a list of all available endpoints.
   #
   # @return [Array] An array of endpoint names.
   def self.list_all_endpoints
-    @list_all_endpoints ||= NseData::APIManager.new.load_endpoints
+    @list_all_endpoints ||= APIManager.new.load_endpoints
   end
 end
+
+NseData.define_api_methods
