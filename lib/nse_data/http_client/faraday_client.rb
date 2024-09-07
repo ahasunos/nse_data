cf0b9427570d8953da97aa11d85272e9d39ed455
Sonu Saha <ahasunos@gmail.com>
2024-09-07 17:13:45 +0530

    enhance: Integrate CachePolicy with FaradayClient for HTTP caching
    - Updated FaradayClient to apply caching based on CachePolicy settings.
    - Configured Faraday to use HTTP caching middleware conditionally.
    
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

--- lib/nse_data/http_client/faraday_client.rb
+++ lib/nse_data/http_client/faraday_client.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'faraday'
+require 'faraday-http-cache'
 require 'json'
 require_relative 'base_client'
 
@@ -8,13 +9,22 @@ module NseData
   module HttpClient
     # FaradayClient class is responsible for making HTTP requests using Faraday.
     class FaradayClient < BaseClient
+      def initialize(base_url, cache_policy)
+        @base_url = base_url
+        @cache_policy = cache_policy
+      end
+
       # Sends a GET request to the specified endpoint.
       #
       # @param endpoint [String] The endpoint to send the request to.
+      # @param force_refresh [Boolean] Whether to force a cache refresh.
       # @return [Faraday::Response] The response object.
-      def get(endpoint)
-        handle_connection do |connection|
-          connection.get(endpoint)
+      def get(endpoint, force_refresh: false)
+        # Use the cache policy to determine whether to fetch from cache or refresh.
+        @cache_policy.fetch(endpoint, force_refresh: force_refresh) do
+          handle_connection(endpoint) do |connection|
+            connection.get(endpoint)
+          end
         end
       end
 
@@ -26,12 +36,21 @@ module NseData
       # @yieldparam connection [Faraday::Connection] The Faraday connection object.
       # @return [Object] The result of the block execution.
       # @raise [Faraday::Error] If there is an error with the connection.
-      def handle_connection
+      def handle_connection(endpoint)
         connection = Faraday.new(url: @base_url) do |faraday|
           faraday.request :json
           faraday.headers['User-Agent'] = 'NSEDataClient/1.0'
           faraday.response :json
           faraday.headers['Accept'] = 'application/json'
+
+          # Check if the cache policy allows caching for this endpoint
+          if @cache_policy.use_cache?(endpoint)
+            # Fetch the TTL (Time-to-Live) for the endpoint
+            ttl = @cache_policy.ttl_for(endpoint)
+            # Apply HTTP caching middleware with the configured store and expiration time
+            faraday.use :http_cache, store: @cache_policy.cache_store, expire_after: ttl
+          end
+
           faraday.adapter Faraday.default_adapter
         end
 
