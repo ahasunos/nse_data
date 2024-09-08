66ff762ada17b19a2305a5161a7d1285ed30086c
Sonu Saha <ahasunos@gmail.com>
2024-09-08 07:40:22 +0530

    refactor: minor refactoring of nse_data module
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

--- lib/nse_data.rb
+++ lib/nse_data.rb
@@ -10,48 +10,52 @@ require_relative 'nse_data/config/logger'
 module NseData
   class Error < StandardError; end
 
-  # This module provides functionality for accessing NSE data.
-  def self.define_api_methods
-    api_manager = APIManager.new
-    api_manager.endpoints.each_key do |method_name|
-      define_singleton_method("fetch_#{method_name}") do
-        api_manager.fetch_data(method_name).body
+  class << self
+    # This module provides functionality for accessing NSE data.
+    def define_api_methods
+      api_manager = APIManager.new
+      api_manager.endpoints.each_key do |method_name|
+        define_singleton_method("fetch_#{method_name}") do
+          api_manager.fetch_data(method_name).body
+        end
       end
     end
-  end
 
-  # Returns a list of all available endpoints.
-  #
-  # @return [Array] An array of endpoint names.
-  def self.list_all_endpoints
-    @list_all_endpoints ||= APIManager.new.load_endpoints
-  end
+    # Returns a list of all available endpoints.
+    #
+    # @return [Array] An array of endpoint names.
+    def list_all_endpoints
+      @list_all_endpoints ||= APIManager.new.load_endpoints
+    end
 
-  # Configure the logger for the NseData gem.
-  #
-  # This method allows users to customize the logger used throughout the library.
-  # To use it, call `NseData.configure` and provide a block to set up the logger.
-  #
-  # Example:
-  #
-  # NseData.configure do |config|
-  #   custom_logger = Logger.new('nse_data.log')
-  #   custom_logger.level = Logger::DEBUG
-  #   config.logger = custom_logger
-  # end
-  #
-  # @yieldparam [NseData::Config::Logger] config The configuration object to be customized.
-  def self.configure
-    yield(@logger_config) if block_given?
-  end
+    # Configure the logger for the NseData gem.
+    #
+    # This method allows users to customize the logger used throughout the library.
+    # To use it, call `NseData.configure` and provide a block to set up the logger.
+    #
+    # Example:
+    #
+    # NseData.configure do |config|
+    #   custom_logger = Logger.new('nse_data.log')
+    #   custom_logger.level = Logger::DEBUG
+    #   config.logger = custom_logger
+    # end
+    #
+    # @yieldparam [NseData::Config::Logger] config The configuration object to be customized.
+    def configure
+      @logger_config ||= Config::Logger.new
+      yield(@logger_config) if block_given?
+    end
 
-  # Access the configured logger.
-  #
-  # This method returns the Logger instance configured through `NseData.configure`.
-  #
-  # @return [Logger] The configured Logger instance.
-  def self.logger
-    @logger_config.logger
+    # Access the configured logger.
+    #
+    # This method returns the Logger instance configured through `NseData.configure`.
+    #
+    # @return [Logger] The configured Logger instance.
+    # @raise [RuntimeError] If the logger has not been configured.
+    def logger
+      @logger_config&.logger || (raise 'Logger not configured. Please call NseData.configure first.')
+    end
   end
 
   # Initialize configuration with default settings.
